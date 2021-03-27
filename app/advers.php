1b436dd4d144bd840483a334d06a22cfb3bf536f
Mr.Programmer <m.mohammadi.developer@gmail.com>
2021-03-27 18:03:02 +0430

    currect advers database name

--- app/advers.php
+++ app/advers.php
@@ -6,11 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Advers extends Model
 {
-    //
+    protected $table = "adverses";
 
     protected $fillable = [
-        'name', 'description', 'price', 'category_id', 'meta_keys', 'meta_description', 'user_id', 'is_confirmed'
-
+        'name',
+        'description',
+        'price',
+        'category_id',
+        'meta_keys', 
+        'meta_description', 
+        'user_id', 
+        'is_confirmed'
     ];
 
+    
+    
 }
