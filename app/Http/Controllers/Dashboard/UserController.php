93707c7eb5103214065951bc75a214e05e0cafac
Mr.Programmer <m.mohammadi.developer@gmail.com>
2021-04-16 22:05:15 +0430

    rename-advers-model

--- app/Http/Controllers/Dashboard/UserController.php
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Dashboard;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
