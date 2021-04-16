0b865fb9fbae20e34dd5a0ebeaecd3dba910bf55
Mr.Programmer <m.mohammadi.developer@gmail.com>
2021-04-16 21:55:31 +0430

    refactor advers controller

--- app/Http/Controllers/Api/AdversController.php
+++ app/Http/Controllers/Api/AdversController.php
@@ -8,9 +8,6 @@ use App\Http\Controllers\Api\Controller;
 
 class AdversController extends Controller
 {
-    //
-
-
     public function index(){
         $post = (object) [
             'titile' => 'post title',
@@ -21,20 +18,16 @@ class AdversController extends Controller
     }
 
     public function userAdverses(){
-        
 
         // $user = $this->authUser();
-
         try {
             $user = auth('api')->userOrFail();
         } catch (\Tymon\JWTAuth\Exceptions\UserNotDefinedException $e) {
             return response()->json(['error' => $e->getMessage() . " :: (User Unknown)"], 401);
         }
 
-
         $post = $this->authUser()->getAdverses();
 
-    
         return response()->json(['adverses' => $post]);
     }
 
@@ -52,7 +45,6 @@ class AdversController extends Controller
             return response()->json(['error' => $e->getMessage() . " :: (User Unknown)"], 401);
         }
 
-
         try {
             $advers = Advers::create([
                 'name' => $details->name,
@@ -63,8 +55,8 @@ class AdversController extends Controller
                 'meta_keys' => $details->meta_keys,
                 'meta_description' => $details->meta_description,
                 'is_confirmed' => '0',
-
             ]);
+
         } catch (\PDOException $e) {
             return response()->json(['error' => $e->getMessage()]);
         }
